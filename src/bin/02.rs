dbab2c19f6c744d7a93b71c4ba6526b8776d1848
tkwu <tkwu@ncsu.edu>
2022-12-10 14:16:00 -0500

    completed day 2 part one

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -1,5 +1,33 @@
 pub fn part_one(input: &str) -> Option<u32> {
-    None
+
+    // Score
+    let mut score: u32 = 0;
+
+    // Iterate lines
+    for line in input.lines() {
+        if line.len() == 3 {
+            // Line as bytes for indexing
+            let line_bytes: &[u8] = line.as_bytes();
+            // Your move
+            let my_move: u8 = line_bytes[2] - 88;       // ASCII offset X
+            // Opps move
+            let opp_move: u8 = line_bytes[0] - 65;      // ASCII offset A
+
+            // Add my move to score
+            score += my_move as u32 + 1;
+
+            // Round result
+            if my_move == opp_move {
+                score += 3;             // Draw
+            } else if (opp_move + 1) % 3 == my_move {   // Use remainder to calculate r<p<s<r cycle
+                score += 6;             // Win
+            }
+        } else {
+            panic!("Invalid line.");
+        }
+    }
+
+    Some(score)
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
@@ -19,7 +47,7 @@ mod tests {
     #[test]
     fn test_part_one() {
         let input = advent_of_code::read_file("examples", 2);
-        assert_eq!(part_one(&input), None);
+        assert_eq!(part_one(&input), Some(15));
     }
 
     #[test]
