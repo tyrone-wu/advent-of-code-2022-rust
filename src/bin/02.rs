ec04fb9e421f14fecad8dbfc0c1006c2e3cbe161
tkwu <tkwu@ncsu.edu>
2022-12-10 14:59:56 -0500

    completed day 2 part two

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -31,7 +31,43 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    
+    // Score
+    let mut score: u32 = 0;
+
+    // Iterate lines
+    for line in input.lines() {
+        if line.len() == 3 {
+            // Line as bytes for indexing
+            let line_bytes: &[u8] = line.as_bytes();
+            // Opps move
+            let opp_move: u8 = line_bytes[0] - 65;      // ASCII offset A
+            // Round result
+            let round_result: u8 = line_bytes[2] - 88;       // ASCII offset X
+
+            // Add round result to score
+            score += round_result as u32 * 3;
+
+            // Round result
+            // if round_result == 0 {          // Lose
+            //     score += (opp_move as u32 + 2) % 3 + 1;
+            // } else if round_result == 1 {   // Draw
+            //     score += opp_move as u32 + 1;
+            // } else if round_result == 2 {   // Win
+            //     score += (opp_move as u32 + 1) % 3 + 1;
+            // }
+            match round_result {
+                0 => score += (opp_move as u32 + 2) % 3 + 1,        // Lose
+                1 => score += opp_move as u32 + 1,                  // Draw
+                2 => score += (opp_move as u32 + 1) % 3 + 1,        // Win
+                _ => panic!("Round result invalid."),
+            }
+        } else {
+            panic!("Invalid line.");
+        }
+    }
+
+    Some(score)
 }
 
 fn main() {
@@ -53,6 +89,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let input = advent_of_code::read_file("examples", 2);
-        assert_eq!(part_two(&input), None);
+        assert_eq!(part_two(&input), Some(12));
     }
 }
