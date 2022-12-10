10272c3576bab872877d24dda1248e65d42305b8
tkwu <tkwu@ncsu.edu>
2022-12-10 15:04:49 -0500

    fixed format

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -1,5 +1,4 @@
 pub fn part_one(input: &str) -> Option<u32> {
-
     // Score
     let mut score: u32 = 0;
 
@@ -9,18 +8,19 @@ pub fn part_one(input: &str) -> Option<u32> {
             // Line as bytes for indexing
             let line_bytes: &[u8] = line.as_bytes();
             // Your move
-            let my_move: u8 = line_bytes[2] - 88;       // ASCII offset X
-            // Opps move
-            let opp_move: u8 = line_bytes[0] - 65;      // ASCII offset A
+            let my_move: u8 = line_bytes[2] - 88; // ASCII offset X
+                                                  // Opps move
+            let opp_move: u8 = line_bytes[0] - 65; // ASCII offset A
 
             // Add my move to score
             score += my_move as u32 + 1;
 
             // Round result
             if my_move == opp_move {
-                score += 3;             // Draw
-            } else if (opp_move + 1) % 3 == my_move {   // Use remainder to calculate r<p<s<r cycle
-                score += 6;             // Win
+                score += 3; // Draw
+            } else if (opp_move + 1) % 3 == my_move {
+                // Use remainder to calculate r<p<s<r cycle
+                score += 6; // Win
             }
         } else {
             panic!("Invalid line.");
@@ -31,7 +31,6 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    
     // Score
     let mut score: u32 = 0;
 
@@ -41,9 +40,9 @@ pub fn part_two(input: &str) -> Option<u32> {
             // Line as bytes for indexing
             let line_bytes: &[u8] = line.as_bytes();
             // Opps move
-            let opp_move: u8 = line_bytes[0] - 65;      // ASCII offset A
-            // Round result
-            let round_result: u8 = line_bytes[2] - 88;       // ASCII offset X
+            let opp_move: u8 = line_bytes[0] - 65; // ASCII offset A
+                                                   // Round result
+            let round_result: u8 = line_bytes[2] - 88; // ASCII offset X
 
             // Add round result to score
             score += round_result as u32 * 3;
@@ -57,9 +56,9 @@ pub fn part_two(input: &str) -> Option<u32> {
             //     score += (opp_move as u32 + 1) % 3 + 1;
             // }
             match round_result {
-                0 => score += (opp_move as u32 + 2) % 3 + 1,        // Lose
-                1 => score += opp_move as u32 + 1,                  // Draw
-                2 => score += (opp_move as u32 + 1) % 3 + 1,        // Win
+                0 => score += (opp_move as u32 + 2) % 3 + 1, // Lose
+                1 => score += opp_move as u32 + 1,           // Draw
+                2 => score += (opp_move as u32 + 1) % 3 + 1, // Win
                 _ => panic!("Round result invalid."),
             }
         } else {
