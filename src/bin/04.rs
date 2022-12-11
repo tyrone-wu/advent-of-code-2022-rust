08001435a1285a684a6c77decde7503be0529a90
tkwu <tkwu@ncsu.edu>
2022-12-11 01:24:06 -0500

    completed day 4 part two

--- src/bin/04.rs
+++ src/bin/04.rs
@@ -28,7 +28,30 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    // Number of pairs
+    let mut pairs: u32 = 0;
+
+    // Iterate lines
+    for line in input.lines() {
+        // Split elfs
+        let elfs: Vec<&str> = line.split(',').collect();
+
+        // Get elf one and elf two values
+        let elf_one: Vec<u8> = elfs[0]
+            .split('-')
+            .map(|n| n.parse::<u8>().unwrap())
+            .collect();
+        let elf_two: Vec<u8> = elfs[1]
+            .split('-')
+            .map(|n| n.parse::<u8>().unwrap())
+            .collect();
+
+        if (elf_one[1] >= elf_two[0]) && (elf_two[1] >= elf_one[0]) {
+            pairs += 1;
+        }
+    }
+
+    Some(pairs)
 }
 
 fn main() {
@@ -50,6 +73,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let input = advent_of_code::read_file("examples", 4);
-        assert_eq!(part_two(&input), None);
+        assert_eq!(part_two(&input), Some(4));
     }
 }
