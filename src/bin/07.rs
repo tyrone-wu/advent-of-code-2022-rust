caac4a52561c6fc2a0acb78a6ecaf440414af64e
tkwu <tkwu@ncsu.edu>
2022-12-14 00:07:38 -0500

    completed day 7 part one

--- src/bin/07.rs
+++ src/bin/07.rs
@@ -1,8 +1,143 @@
-pub fn part_one(input: &str) -> Option<u32> {
-    None
+use std::collections::{BTreeMap, VecDeque};
+
+// Directory struct
+struct Directory {
+    // name: String,                             // Name of dir
+    parent_dir: Option<usize>,                // Index of parent dir
+    size: u64,                                // Size of dir
+    subdirs: Option<BTreeMap<String, usize>>, // Key pairs of dir name with their index
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
+impl Directory {
+    fn add_size(&mut self, add_size: u64) {
+        self.size += add_size;
+    }
+
+    fn set_subdirs(&mut self, subdirs: BTreeMap<String, usize>) {
+        self.subdirs = Some(subdirs);
+    }
+}
+
+// ----------------------------------------------------------------------------
+
+pub fn part_one(input: &str) -> Option<u64> {
+    // Stores the filesystem structure with only sizes
+    let mut filesystem: VecDeque<Directory> = VecDeque::new();
+
+    // Add root dir
+    filesystem.push_back(Directory {
+        // name: String::from("/"),
+        parent_dir: None,
+        size: 0,
+        subdirs: None,
+    });
+    // Initial position of the filesystem
+    let mut curr_position: usize = 0;
+
+    // Iterate over cli commands
+    let cli_it = input.split("\n$ ").skip(1);
+    for command in cli_it {
+        // Line iterator
+        let command_vec: Vec<&str> = command.lines().collect();
+        let command_type: &str = command_vec[0];
+
+        if command_type.starts_with("cd") {
+            // Get dir name to change into
+            let dir_name: &str = command_type.rsplit_once(' ').unwrap().1;
+
+            // Set current directory of the filesystem
+            match dir_name {
+                "/" => curr_position = 0,
+                ".." => curr_position = filesystem[curr_position].parent_dir.unwrap(),
+                _ => {
+                    curr_position = *filesystem[curr_position]
+                        .subdirs
+                        .clone()
+                        .unwrap()
+                        .get(dir_name)
+                        .unwrap()
+                }
+            }
+        } else if command_type.starts_with("ls") {
+            // Ensures that if "ls" output is not double counted
+            if filesystem[curr_position].subdirs.is_none() {
+                // If "ls" output is not empty
+                if command_vec.len() != 1 {
+                    // Store subdirs
+                    let mut subdirs: BTreeMap<String, usize> = BTreeMap::new();
+                    // Store size
+                    let mut dir_size: u64 = 0;
+
+                    // Parse "ls" output
+                    for out in command_vec[1..].iter() {
+                        // Output is dir
+                        if out.starts_with("dir") {
+                            // Get dir name
+                            let dir_name: &str = out.split_whitespace().last().unwrap();
+                            // Index of the dir to insert
+                            let insert_idx: usize = filesystem.len();
+
+                            // Insert sub dir
+                            filesystem.push_back(Directory {
+                                // name: dir_name.to_string(),
+                                parent_dir: Some(curr_position),
+                                size: 0,
+                                subdirs: None,
+                            });
+
+                            // Record inserted sub dir
+                            subdirs.insert(dir_name.to_string(), insert_idx);
+                        } else {
+                            // Output is file
+                            let file_size: u64 = out
+                                .split_whitespace()
+                                .next()
+                                .unwrap()
+                                .parse::<u64>()
+                                .unwrap();
+                            dir_size += file_size;
+                        }
+                    }
+
+                    // Set sub dirs and size
+                    filesystem[curr_position].set_subdirs(subdirs);
+                    filesystem[curr_position].add_size(dir_size);
+
+                    // Propogate dir size to parent dirs
+                    let mut parent_position: Option<usize> = filesystem[curr_position].parent_dir;
+                    while parent_position.is_some() {
+                        // Add sub dir size to parent dir
+                        filesystem[parent_position.unwrap()].add_size(dir_size);
+                        // Move to parent dir
+                        parent_position = filesystem[parent_position.unwrap()].parent_dir;
+                    }
+                }
+            }
+        }
+    }
+
+    // Calculate sum of sizes < 100000
+    let mut total_size: u64 = 0;
+    for d in filesystem.iter() {
+        // println!("--- Directory ---");
+        // println!("Index: {i}");
+        // println!("Name: {}", d.name);
+        // println!("Parent Directory: {}", d.parent_dir.unwrap_or(0));
+        // println!("Size: {}", d.size);
+        // println!("------ Subdir ---");
+        // for (n, di) in d.subdirs.clone().unwrap() {
+        //     println!("    Name: {n}\tIndex: {di}");
+        // }
+
+        if d.size <= 100000 {
+            total_size += d.size;
+        }
+    }
+
+    Some(total_size)
+}
+
+pub fn part_two(_input: &str) -> Option<u32> {
     None
 }
 
@@ -19,7 +154,7 @@ mod tests {
     #[test]
     fn test_part_one() {
         let input = advent_of_code::read_file("examples", 7);
-        assert_eq!(part_one(&input), None);
+        assert_eq!(part_one(&input), Some(95437));
     }
 
     #[test]
